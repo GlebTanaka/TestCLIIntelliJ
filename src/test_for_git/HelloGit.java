47fb8eea139550ff4e67e228fa0a3de4181aa812
GlebTanaka <69252964+GlebTanaka@users.noreply.github.com>
2020-09-06 18:57:25 +0200

    Update HelloGit.java

--- src/test_for_git/HelloGit.java
+++ src/test_for_git/HelloGit.java
@@ -3,6 +3,7 @@ package test_for_git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello Git!");
-        System.out.println("I have made some changes");
+        System.out.println("I have made some changes from IntelliJ");
+        System.out.println("now I have changed from GitHub);
     }
 }
