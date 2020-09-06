d7006f3d093b629bc35dd44b15c9766b6de2234f
GlebTanaka <gleb5655@gmail.com>
2020-09-06 18:55:20 +0200

    added a massage

--- src/test_for_git/HelloGit.java
+++ src/test_for_git/HelloGit.java
@@ -3,5 +3,6 @@ package test_for_git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("I have made some changes");
     }
 }
